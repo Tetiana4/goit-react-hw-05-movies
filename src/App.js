db284b7babfc60619682d89e4161bc75e62fe837
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-09-24 22:54:15 +0200

    Add start code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import HomePage from './views/HomePage';
+import MoviesPage from './views/MoviesPage';
+import NotFound from './views/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/movies">Movies</NavLink>
+      </li>
+    </ul>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/movies" component={MoviesPage} />
+      <Route component={NotFound} />
+    </Switch>
+  </>
+);
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+// import axios from 'axios';
+// import PropTypes from 'prop-types';
+
+// axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+// const KEY = 'a97a44bb2f674ee3272cc719c5c287d9';
+
+// export const fetchTrendMovies = async () => {
+//   const { data } = await axios.get(`/trending/all/day?api_key=${KEY}`);
+//   return data;
+// };
